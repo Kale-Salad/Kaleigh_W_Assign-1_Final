1087dd6718dd060ef59e5ee6ca391f2752c508ba
Watson <kaleigh.watson@uleth.ca>
2020-01-30 17:56:46 -0700

    more framework 7 plus a page

---
+++ www/js/myapp.js
@@ -0,0 +1,18 @@
+var app = new Framework7({
+    
+    root: '#app',
+    routes: [
+    {
+    path: '/',
+    url: 'index.html'
+},
+                         {
+    
+    path: '/page1/',
+    url: 'pages/page1.html'
+}
+    ]
+    
+})
+
+var mainView = app.views.create(".view-main");
